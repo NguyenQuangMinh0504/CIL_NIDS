ec92524ec6cce9d22db8a7c337576cd85b29d851
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-12-16 09:37:10 +0700

    Update der.py

--- Code/PythonCode/CIL/models/der.py
+++ Code/PythonCode/CIL/models/der.py
@@ -12,7 +12,9 @@ from utils.data_manager import DataManager
 from utils.toolkit import count_parameters, tensor2numpy
 
 
+init_epoch = 200
 init_lr = 0.1
+
 batch_size = 128
 num_workers = 8
 init_weight_decay = 0.0005
@@ -53,6 +55,10 @@ class DER(BaseLearner):
         self.test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
         if len(self._multiple_gpus) > 1:
             self._network == nn.DataParallel(self._network, self._multiple_gpus)
+        self._train(self.train_loader, self.test_loader)
+        self.build_rehearsal_memory(data_manager, self.samples_per_class)
+        if len(self._multiple_gpus) > 1:
+            self._network = self._network.module
 
     def _train(self, train_loader, test_loader):
         logging.info("Calling function _train ....")
@@ -100,6 +106,50 @@ class DER(BaseLearner):
         if self._cur_task >= 1:
             for i in range(self._cur_task):
                 self._network_module_ptr.convnets[i].eval()
+
+    def _init_train(self, train_loader, test_loader, optimizer, scheduler):
+        prog_bar = tqdm(range(init_epoch))
+        for _, epoch in enumerate(prog_bar):
+            self.train()
+            losses = 0.0
+            correct, total = 0, 0
+            for i, (_, inputs, targets) in enumerate(train_loader):
+                inputs, targets = inputs.to(self._device), targets.to(self._device)
+                logits = self._network(inputs)["logits"]
+                loss = F.cross_entropy(logits, targets)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                losses += loss.item()
+
+                _, preds = torch.max(logits, dim=1)
+                correct += preds.eq(targets.expand_as(preds)).cpu().sum()
+                total += len(targets)
+
+            scheduler.step()
+            train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+
+            if epoch % 5 == 0:
+                test_acc = self._compute_accuracy(self._network, test_loader)
+                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    init_epoch,
+                    losses / len(train_loader),
+                    train_acc,
+                    test_acc,
+                )
+            else:
+                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    init_epoch,
+                    losses / len(train_loader),
+                    train_acc,
+                )
+            prog_bar.set_description(info)
+
+        # logging.info(info)
 
     def _update_representation(self, train_loader, test_loader, optimizer: optim.SGD, scheduler):
         prog_bar = tqdm(range(epochs))
