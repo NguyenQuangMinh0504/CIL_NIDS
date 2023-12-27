57f7ff1613f6cdbf9bdbd7da3683d3984632c143
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-12-27 20:31:06 +0700

    .

--- Code/PythonCode/CIL/scripts/benchmark/ton_iot_network_b0_5_finetune.sh
+++ Code/PythonCode/CIL/scripts/benchmark/ton_iot_network_b0_5_finetune.sh
@@ -1,8 +1,8 @@
-python3.9 main.py \
+nohup python3.9 main.py \
     -model finetune \
     --dataset ton-iot-network \
     -net ton_iot_network_ann \
     -init 5 \
     -incre 5 \
     -p benchmark \
-    -d -1
+    -d -1 &
