ce754c82cd6d8a8c49e90dcaa7086df9bb8fc7e0
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-01-08 13:10:11 +0700

    .

---
+++ Code/PythonCode/CIL/scripts/benchmark/cic_ids_2017_b0_2_der.sh
@@ -0,0 +1,8 @@
+python3.9 main.py \
+    -model der \
+    --dataset cic-ids-2017 \
+    -net cic_ids_ann \
+    -init 2 \
+    -incre 2 \
+    -p benchmark \
+    -d -1
---
+++ Code/PythonCode/CIL/scripts/benchmark/cic_ids_2017_b0_2_finetune.sh
@@ -0,0 +1,6 @@
+nohup python3.9 main.py \
+    -model finetune \
+    -init 2 -incre 2 \
+    --dataset cic-ids-2017 \
+    -net cic_ids_ann \
+    -p benchmark -d -1 &
