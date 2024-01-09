5cf9331b7a11ef92b08945dbeec81e71f52c97dd
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-01-09 13:35:44 +0700

    .

--- Code/PythonCode/CIL/utils/data_manager.py
+++ Code/PythonCode/CIL/utils/data_manager.py
@@ -185,7 +185,6 @@ class DummyDataset(Dataset):
 
 
 def _map_new_class_index(y, order: list):
-    logging.info(f"type of order is: {type(order)}")
     return np.array(list(map(lambda x: order.index(x), y)))
 
 
