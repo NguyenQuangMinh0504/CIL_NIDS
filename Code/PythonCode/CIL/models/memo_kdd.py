4c2ee52575edc383f160a7c48b3756ac2b05993a
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-01-22 16:06:28 +0700

    Update memo_kdd.py

--- Code/PythonCode/CIL/models/memo_kdd.py
+++ Code/PythonCode/CIL/models/memo_kdd.py
@@ -5,8 +5,12 @@ import torch
 from torch import nn
 from torch import optim
 from torch.nn import functional as F
+
 from models.base import BaseLearner
+
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard.writer import SummaryWriter
+
 from utils.inc_net import AdaptiveKDDNet
 from utils.data_manager import DataManager
 from utils.toolkit import count_parameters, tensor2numpy
@@ -177,7 +181,7 @@ class MEMO_KDD(BaseLearner):
     def _init_train(self, train_loader: DataLoader, test_loader: DataLoader, optimizer, scheduler):
 
         logging.info("Initialize training.........................")
-
+        writer = SummaryWriter(log_dir="runs/Memo/Task{}".format(self._cur_task))
         prog_bar = tqdm(range(self.args["init_epoch"]))
         for _, epoch in enumerate(prog_bar):
             self._network.train()
@@ -200,10 +204,13 @@ class MEMO_KDD(BaseLearner):
             scheduler.step()
 
             train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+            writer.add_scalar("Loss/train", losses, epoch)
+            writer.add_scalar("Accuracy/train", train_acc, epoch)
 
             # Generate info message
             if epoch % 5 == 0:
                 test_acc = self._compute_accuracy(self._network, test_loader)
+                writer.add_scalar("Accuracy/Test", test_acc, epoch)
                 info = 'Task {}, Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}'.format(
                     self._cur_task, epoch + 1,
                     self.args["init_epoch"],
@@ -218,6 +225,7 @@ class MEMO_KDD(BaseLearner):
     def _update_representation(self, train_loader: DataLoader,
                                test_loader: DataLoader, optimizer: optim.SGD, scheduler):
         prog_bar = tqdm(range(self.args["epochs"]))
+        writer = SummaryWriter(log_dir="runs/Memo/Task{}".format(self._cur_task))
         for _, epoch in enumerate(prog_bar):
             self.set_network()
             losses = 0.
@@ -250,8 +258,11 @@ class MEMO_KDD(BaseLearner):
             scheduler.step()
 
             train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+            writer.add_scalar("Loss/train", losses, epoch)
+            writer.add_scalar("Accuracy/train", train_acc, epoch)
             if epoch % 5 == 0:
                 test_acc = self._compute_accuracy(self._network, test_loader)
+                writer.add_scalar("Accuracy/Test", test_acc, epoch)
                 info = "Task {}, Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
                     self._cur_task,
                     epoch + 1,
