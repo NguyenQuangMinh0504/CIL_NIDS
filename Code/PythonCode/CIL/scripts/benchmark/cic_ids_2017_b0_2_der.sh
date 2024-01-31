067b6de8f5447cb10179d1736cb2799eda15ab09
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-01-31 13:21:47 +0700

    .

--- Code/PythonCode/CIL/scripts/benchmark/cic_ids_2017_b0_2_der.sh
+++ Code/PythonCode/CIL/scripts/benchmark/cic_ids_2017_b0_2_der.sh
@@ -1,11 +1,11 @@
     python3.9 main.py \
         -model der \
         --dataset cic-ids-2017 \
-        -net cic_ids_dnn \
+        -net cic_ids_ann \
         -init 2 \
         -incre 2 \
         -p benchmark \
         -d -1 \
         --init_epoch 300 \
         --epochs 300 \
-        --batch_size 512
+        --batch_size 128
