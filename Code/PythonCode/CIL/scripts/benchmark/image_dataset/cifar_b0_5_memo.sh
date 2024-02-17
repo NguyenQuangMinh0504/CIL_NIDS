a2392cdf2dbb43bfb43e0c6e054e52c8bf0380d9
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-17 14:14:41 +0700

    .

---
+++ Code/PythonCode/CIL/scripts/benchmark/image_dataset/cifar_b0_5_memo.sh
@@ -0,0 +1,10 @@
+python main_memo.py \
+    -model memo \
+    -init 5 \
+    -incre 5 \
+    -net memo_resnet32 \
+    -p benchmark \
+    -d -1 \
+    --scheduler cosine \
+    --t_max 170 \
+    --train_base \
