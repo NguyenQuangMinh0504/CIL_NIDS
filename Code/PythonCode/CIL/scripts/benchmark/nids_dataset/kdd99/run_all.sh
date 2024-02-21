846a2d5291793458e1e29ee283c9e5c7af11f675
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-21 11:06:35 +0700

    .

---
+++ Code/PythonCode/CIL/scripts/benchmark/nids_dataset/kdd99/run_all.sh
@@ -0,0 +1,4 @@
+scripts/benchmark/nids_dataset/kdd99/kdd99_b0_2_der.sh &&
+scripts/benchmark/nids_dataset/kdd99/kdd99_b0_2_finetune.sh &&
+scripts/benchmark/nids_dataset/kdd99/kdd99_b0_2_memo.sh &&
+scripts/benchmark/nids_dataset/kdd99/kdd99_b0_2_lwf.sh 
