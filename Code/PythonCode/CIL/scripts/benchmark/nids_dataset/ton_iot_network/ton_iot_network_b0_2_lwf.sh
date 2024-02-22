f033ac234feb3d234fb520252d5e0351fc18cdcc
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-22 11:33:43 +0700

    .

---
+++ Code/PythonCode/CIL/scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_lwf.sh
@@ -0,0 +1,12 @@
+python3 main.py \
+    -model lwf \
+    --dataset ton-iot-network \
+    -ms 2000 \
+    -init 2 \
+    -incre 2 \
+    -net ton_iot_network_ann \
+    -p benchmark \
+    --init_epoch 10 \
+    --epochs 10 \
+    --batch_size 128 \
+    -d -1
