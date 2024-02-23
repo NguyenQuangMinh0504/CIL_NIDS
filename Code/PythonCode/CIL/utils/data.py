383a326930a6f7e9c007d4801fe3a1ff2eef79c7
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-23 17:50:02 +0700

    .

--- Code/PythonCode/CIL/utils/data.py
+++ Code/PythonCode/CIL/utils/data.py
@@ -73,9 +73,7 @@ class KDD99(iData):
     common_trsf = [ToTensor()]
 
     def __init__(self, **kwargs):
-        print(type(kwargs))
-        print(kwargs)
-        print(kwargs["pre_processing"])
+        self.pre_processing = kwargs["pre_processing"]
 
     def download_data(self):
         super().download_data()
@@ -99,67 +97,6 @@ class KDD99(iData):
                       'dst_host_serror_rate', 'dst_host_srv_serror_rate', 'dst_host_rerror_rate',
                       'dst_host_srv_rerror_rate', 'outcome']
 
-        # logging.info(df["protocol_type"].value_counts(normalize=False, sort=True))
-        # smurf_df = df[df["outcome"] == "smurf."]
-        # normal_df = df[df["outcome"] == "normal."]
-        # neptune_df = df[df["outcome"] == "neptune."]
-        # back_df = df[df["outcome"] == "back."]
-        # pod_df = df[df["outcome"] == "pod."]
-
-        # logging.info("Protocol type ....")
-        # logging.info(df["protocol_type"].value_counts(normalize=False, sort=True))
-        # logging.info("smurf protocol type ....")
-        # logging.info(smurf_df["protocol_type"].value_counts(normalize=False, sort=True))
-        # logging.info("normal protocol type ....")
-        # logging.info(normal_df["protocol_type"].value_counts(normalize=False, sort=True))
-
-        # info of service attribute
-        # logging.info(df["service"].unique())
-        # logging.info("service value counts...")
-        # logging.info(df["service"].value_counts(normalize=False, sort=True))
-        # logging.info("service smurf ...")
-        # logging.info(smurf_df["service"].value_counts(normalize=False, sort=True))
-        # logging.info("service normal ...")
-        # logging.info(normal_df["service"].value_counts(normalize=False, sort=True))
-
-        # logging.info("outcome....")
-        # logging.info(df["outcome"].value_counts(normalize=False, sort=True))
-        # logging.info("src bytes...")
-        # logging.info(df["src_bytes"].value_counts(normalize=False, sort=True))
-        # logging.info("src bytes statistic ... ")
-        # logging.info(df["src_bytes"].describe(percentiles=[0, 0.5, 0.99]))
-        # logging.info("smurf src bytes ...")
-        # logging.info(smurf_df["src_bytes"].value_counts(normalize=False, sort=True))
-        # plt.scatter(df["src_bytes"], df["src_bytes"])
-
-        # info of flag attribute
-        # logging.info("Flag..................")
-        # logging.info(df["flag"].unique())
-        # logging.info(df["flag"].value_counts(normalize=False, sort=True))
-        # logging.info(smurf_df["flag"].value_counts(normalize=False, sort=True))
-        # logging.info(normal_df["flag"].value_counts(normalize=False, sort=True))
-
-        # # info of source bytes attribute
-        # logging.info("Src byte.............")
-        # logging.info(df["src_bytes"].unique())
-        # logging.info(df["src_bytes"].value_counts(normalize=False, sort=True))
-        # logging.info(smurf_df["src_bytes"].value_counts(normalize=False, sort=True))
-        # logging.info(normal_df["src_bytes"].value_counts(normalize=False, sort=True))
-
-        # # info of neptune source bytes
-        # logging.info("Source bytes of neptune dataset......")
-        # logging.info(f"Len of neptune df is: {len(neptune_df)}")
-        # logging.info(neptune_df["src_bytes"].value_counts(normalize=False, sort=True))
-        # logging.info(neptune_df["dst_bytes"].value_counts(normalize=False, sort=True))
-
-        # info of back attack
-        # logging.info(back_df["src_bytes"].value_counts(normalize=False, sort=True))
-
-        # logging.info("Wrong fragment...")
-        # logging.info(df["wrong_fragment"].value_counts(normalize=False, sort=True))
-        # logging.info("Pod wrong fragment")
-        # logging.info(pod_df["wrong_fragment"].value_counts(normalize=False, sort=True))
-
         # ---------------- Droping all value that has less than 200 records.
         y_drop = ["spy.", "perl.", "phf.", "multihop.",
                   "ftp_write.", "loadmodule.", "rootkit.", "imap.",
@@ -169,47 +106,24 @@ class KDD99(iData):
 
         logging.info(df["outcome"].value_counts())
 
-        encode_numeric_zscore(df, 'duration')
+        for name in ["duration", "src_bytes", "dst_bytes", "wrong_fragment", "urgent", "hot",
+                     "num_failed_logins", "num_compromised", "root_shell", "su_attempted",
+                     "num_root", "num_file_creations", "num_shells", "num_access_files",
+                     "num_outbound_cmds", "count", "srv_count", "serror_rate", "srv_serror_rate",
+                     "rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate",
+                     "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count",
+                     "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate",
+                     "dst_host_srv_diff_host_rate", "dst_host_serror_rate",
+                     "dst_host_srv_serror_rate", "dst_host_rerror_rate", "dst_host_srv_rerror_rate"]:
+            encode_numeric_zscore(df, name=name)
+
         encode_text_dummy(df, 'protocol_type')
         encode_text_dummy(df, 'service')
         encode_text_dummy(df, 'flag')
-        encode_numeric_zscore(df, 'src_bytes')
-        encode_numeric_zscore(df, 'dst_bytes')
         encode_text_dummy(df, 'land')
-        encode_numeric_zscore(df, 'wrong_fragment')
-        encode_numeric_zscore(df, 'urgent')
-        encode_numeric_zscore(df, 'hot')
-        encode_numeric_zscore(df, 'num_failed_logins')
         encode_text_dummy(df, 'logged_in')
-        encode_numeric_zscore(df, 'num_compromised')
-        encode_numeric_zscore(df, 'root_shell')
-        encode_numeric_zscore(df, 'su_attempted')
-        encode_numeric_zscore(df, 'num_root')
-        encode_numeric_zscore(df, 'num_file_creations')
-        encode_numeric_zscore(df, 'num_shells')
-        encode_numeric_zscore(df, 'num_access_files')
-        encode_numeric_zscore(df, 'num_outbound_cmds')
         encode_text_dummy(df, 'is_host_login')
         encode_text_dummy(df, 'is_guest_login')
-        encode_numeric_zscore(df, 'count')
-        encode_numeric_zscore(df, 'srv_count')
-        encode_numeric_zscore(df, 'serror_rate')
-        encode_numeric_zscore(df, 'srv_serror_rate')
-        encode_numeric_zscore(df, 'rerror_rate')
-        encode_numeric_zscore(df, 'srv_rerror_rate')
-        encode_numeric_zscore(df, 'same_srv_rate')
-        encode_numeric_zscore(df, 'diff_srv_rate')
-        encode_numeric_zscore(df, 'srv_diff_host_rate')
-        encode_numeric_zscore(df, 'dst_host_count')
-        encode_numeric_zscore(df, 'dst_host_srv_count')
-        encode_numeric_zscore(df, 'dst_host_same_srv_rate')
-        encode_numeric_zscore(df, 'dst_host_diff_srv_rate')
-        encode_numeric_zscore(df, 'dst_host_same_src_port_rate')
-        encode_numeric_zscore(df, 'dst_host_srv_diff_host_rate')
-        encode_numeric_zscore(df, 'dst_host_serror_rate')
-        encode_numeric_zscore(df, 'dst_host_srv_serror_rate')
-        encode_numeric_zscore(df, 'dst_host_rerror_rate')
-        encode_numeric_zscore(df, 'dst_host_srv_rerror_rate')
 
         # logging.info(df["outcome"].value_counts(normalize=False, sort=True))
         # logging.info((df[df["outcome"] == "smurf."]))
