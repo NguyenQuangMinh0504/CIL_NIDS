258806b60ace091c021e8b4d18c00f92330f920f
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-03-03 18:40:16 +0700

    add script icarl ton-iot-net dataset

---
+++ Code/PythonCode/CIL/scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_icarl.sh
@@ -0,0 +1,9 @@
+python main.py \
+    -model icarl \
+    --dataset ton-iot-network \
+    -ms 2000 \
+    -init 2 \
+    -incre 2 \
+    -net ton_iot_network_memo_ann \
+    -p benchmark \
+    -d -1 
