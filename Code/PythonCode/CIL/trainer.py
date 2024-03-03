c2ac9520e300b762cf3e66c461ffc3e4d79b0e4c
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-03-03 19:48:53 +0700

    fix icarl

--- Code/PythonCode/CIL/trainer.py
+++ Code/PythonCode/CIL/trainer.py
@@ -135,7 +135,7 @@ def _train(args: dict):
             logging.info("NME top1 curve: {}".format(nme_curve["top1"]))
             logging.info("NME top5 curve: {}\n".format(nme_curve["top5"]))
 
-            writer = SummaryWriter(log_dir="runs/1000/{}/{}/{}_{}/Accuracy_curve".format(
+            writer = SummaryWriter(log_dir="runs/{}/{}/{}_{}/Accuracy_curve".format(
                 args["dataset"],
                 args["model_name"],
                 args["convnet_type"],
