ebc911c10f6c403c7b3c98e177164ee0c4894b75
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-03-03 19:52:58 +0700

    update script

--- Code/PythonCode/CIL/scripts/benchmark/nids_dataset/ton_iot_network/run_all.sh
+++ Code/PythonCode/CIL/scripts/benchmark/nids_dataset/ton_iot_network/run_all.sh
@@ -1,4 +1,5 @@
 scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_der.sh &&
 scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_memo.sh &&
 scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_finetune.sh &&
-scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_lwf.sh 
+scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_lwf.sh &&
+scripts/benchmark/nids_dataset/ton_iot_network/ton_iot_network_b0_2_icarl.sh &&
