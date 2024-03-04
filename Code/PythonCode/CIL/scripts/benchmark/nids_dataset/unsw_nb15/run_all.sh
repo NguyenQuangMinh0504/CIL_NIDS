5cf117c3c68fd4726fd443af9c340279acfb96be
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-03-05 00:57:36 +0700

    .

--- Code/PythonCode/CIL/scripts/benchmark/nids_dataset/unsw_nb15/run_all.sh
+++ Code/PythonCode/CIL/scripts/benchmark/nids_dataset/unsw_nb15/run_all.sh
@@ -1,4 +1,5 @@
 scripts/benchmark/nids_dataset/unsw_nb15/unsw_nb15_b0_2_finetune.sh &&
 scripts/benchmark/nids_dataset/unsw_nb15/unsw_nb15_b0_2_memo.sh &&
 scripts/benchmark/nids_dataset/unsw_nb15/unsw_nb15_b0_2_der.sh &&
-scripts/benchmark/nids_dataset/unsw_nb15/unsw_nb15_b0_2_lwf.sh
+scripts/benchmark/nids_dataset/unsw_nb15/unsw_nb15_b0_2_lwf.sh &&
+scripts/benchmark/nids_dataset/unsw_nb15/unsw_nb15_b0_2_icarl.sh
