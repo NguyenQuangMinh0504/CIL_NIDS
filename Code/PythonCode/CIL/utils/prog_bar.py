8d8b37755777661fcd33771ad4e8c18a85b0f038
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-03-04 23:26:50 +0700

    add option not using telgram bar based on env variable

--- Code/PythonCode/CIL/utils/prog_bar.py
+++ Code/PythonCode/CIL/utils/prog_bar.py
@@ -1,6 +1,12 @@
-from tqdm.contrib.telegram import trange
+from tqdm.contrib import telegram
+from tqdm import trange
+import os
+
 from setting import BOT_API_TOKEN, CHAT_ROOM_ID
 
 
 def prog_bar(epochs):
-    return trange(epochs, token=BOT_API_TOKEN, chat_id=CHAT_ROOM_ID, mininterval=3)
+    TELEGRAM_USING = os.getenv("TELEGRAM_USING", default=0)
+    if int(TELEGRAM_USING) == 0:
+        return trange(epochs)
+    return telegram.trange(epochs, token=BOT_API_TOKEN, chat_id=CHAT_ROOM_ID, mininterval=3)
