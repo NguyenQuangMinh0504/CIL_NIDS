2efcbc9821aff228146c3e6c6743876210ef3cb3
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-03-26 23:55:35 +0700

    update stuff

--- Code/PythonCode/CIL/utils/data.py
+++ Code/PythonCode/CIL/utils/data.py
@@ -104,7 +104,7 @@ class KDD99(iData):
         logging.info(df.columns[df.isna().any()])
         logging.info(df.columns[df.isnull().any()])
         logging.info("Duplicate data")
-        logging.info(df.columns[df.duplicated().any()])
+        logging.info(df.duplicated().any())
 
         # ---------------- Droping all value that has less than 200 records.
         y_drop = ["spy.", "perl.", "phf.", "multihop.",
