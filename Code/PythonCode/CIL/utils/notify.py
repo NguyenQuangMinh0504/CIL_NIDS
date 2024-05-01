fa301c44e5a47a7a9d9ebfb96d534fed96cbc40e
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-05-01 21:23:03 +0700

    add comment, catch exception in send telegram message function

--- Code/PythonCode/CIL/utils/notify.py
+++ Code/PythonCode/CIL/utils/notify.py
@@ -2,9 +2,12 @@ from setting import BOT_API_TOKEN, CHAT_ROOM_ID
 import telebot
 
 
-def send_telegram_notification(text: str):
+def send_telegram_notification(text: str) -> None:
+    """Sending telegram message to chat room with id CHAT_ROOM_ID using bot BOT_API_TOKEN in file setting.py"""
     try:
         bot = telebot.TeleBot(BOT_API_TOKEN)
         bot.send_message(chat_id=CHAT_ROOM_ID, text=text)
     except telebot.apihelper.ApiTelegramException:
         print("Error when sending telegram message")
+    except Exception:
+        print("Error sending telegram message")
